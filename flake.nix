b2ef82f9a760a4668ecf1748cdf78889389b60c0
Michal Rus <michal.rus@iohk.io>
2023-09-28 17:49:01 +0200

    Add one legacy combination, and one for SanchoNet

--- flake.nix
+++ flake.nix
@@ -6,11 +6,11 @@
 
   outputs = inputs: let
     matrix = [
-      # ogmiosRef ogmiosHash nodeRef nodeHash
-      [ "v6.0.0-rc2" "sha256-UWw+sOn6k2iwpRanz2XwuEmrM7pjyuGXn1FU0NSUgCQ="
-        "8.3.1-pre" "sha256-64Nc6CKSMe4SoOu1zaqP9XekWWMDsTVRAJ5faEvbkb4=" ]
-      [ "f40a8921906fecae4c52ffff34fb011457f9a771" "sha256-lNe0uK/fH18om8mhMx9jZ2zAQT47u5zNtjU+ZXoUzBY="
-        "8.1.2" "sha256-d0V8N+y/OarYv6GQycGXnbPly7GeJRBEeE1017qj9eI=" ]
+      # ogmiosRef                                   ogmiosHash                                             nodeRef      nodeHash
+      [ "v5.6.0"                                    "sha256-7Hb/ZaEnnMSOPhRlx4YR+cgpZOttYEJNzHBl+RamlZM="  "1.35.5"     "sha256-LwoQejDDA9P7dbl8z3nFbEJDDim0Y7oHUOgf4EH2Xig=" ]
+      [ "f40a8921906fecae4c52ffff34fb011457f9a771"  "sha256-lNe0uK/fH18om8mhMx9jZ2zAQT47u5zNtjU+ZXoUzBY="  "8.3.1-pre"  "sha256-64Nc6CKSMe4SoOu1zaqP9XekWWMDsTVRAJ5faEvbkb4=" ]
+      [ "f40a8921906fecae4c52ffff34fb011457f9a771"  "sha256-lNe0uK/fH18om8mhMx9jZ2zAQT47u5zNtjU+ZXoUzBY="  "8.1.2"      "sha256-d0V8N+y/OarYv6GQycGXnbPly7GeJRBEeE1017qj9eI=" ]
+      [ "v6.0.0-rc2"                                "sha256-UWw+sOn6k2iwpRanz2XwuEmrM7pjyuGXn1FU0NSUgCQ="  "8.3.1-pre"  "sha256-64Nc6CKSMe4SoOu1zaqP9XekWWMDsTVRAJ5faEvbkb4=" ]
     ];
     supportedSystems = [ "x86_64-linux" /*"aarch64-linux"*/ ];
     inherit (inputs.nixpkgs-2305) lib;
